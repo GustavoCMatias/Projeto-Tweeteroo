af2619334331a97b604768bab6fa5adda90a4fe9
GustavoCMatias <gustavocmatias@gmail.com>
2023-01-06 11:51:10 -0300

    Post do sign up configurado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 
+const users = []
+const tweets = []
+
+
+
 const server= express()
 server.use(cors())
+server.use(express.json())
 
 server.listen(5000,() => {console.log('ta funfando')})
+
+server.post('/sign-up', (req, res) => {
+    users.push(req.body)
+    res.send("OK")
+})
